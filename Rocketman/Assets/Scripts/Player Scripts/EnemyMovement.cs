e6ed7d759f4786d71c7b6c4b8f9ac8692b80c355
Ndavis33 <103063746+Ndavis33@users.noreply.github.com>
2022-09-10 09:31:47 -0700

    enemy script
    a few other things too

---
+++ Rocketman/Assets/Scripts/Player Scripts/EnemyMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("Objects")]
+    public GameObject upPoint;
+    public GameObject downPoint;
+    private Vector3 upPos;
+    private Vector3 downPos;
+    [Header("Variables")]
+    public int speed;
+    private bool goingUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        upPos = upPoint.transform.position;
+
+        downPos = downPoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    //move function for enemy left and right
+    private void move()
+    {
+        if (goingUp)
+        {
+            if (transform.position.y >= upPos.y)
+            {
+                goingUp = false;
+            }
+            else
+            {
+                transform.position += Vector3.up * Time.deltaTime * speed;
+            }
+        }
+        else
+        {
+            if (transform.position.y <= downPos.y)
+            {
+                goingUp = true;
+            }
+            else
+            {
+                transform.position += Vector3.down * Time.deltaTime * speed;
+            }
+        }
+    }
+}
--- Rocketman/Assets/Scripts/Test_Stewart.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Test_Stewart : MonoBehaviour
-{
-    //This script is a test to make sure my gitHUb sharing works
-}
