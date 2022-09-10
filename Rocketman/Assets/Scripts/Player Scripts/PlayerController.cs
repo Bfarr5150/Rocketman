3455daf8acc66b57112625d5850dc7988c61adbc
Brad Farris <xtraponagex@live.com>
2022-09-10 09:57:51 -0700

    Camera Follow Script Deleted
    Camera is now child of player

--- Rocketman/Assets/Scripts/Player Scripts/PlayerController.cs
+++ Rocketman/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -6,17 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject player;
     public Rigidbody rocket;
+    public float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+        rocket = player.GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(rotationSpeed * Time.deltaTime, 0, 0);
     }
 }
 
