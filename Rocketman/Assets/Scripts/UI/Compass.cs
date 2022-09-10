7ad0c261178b46da39aeea0ee6ee9591215ed358
Michael Szolowicz <michaelszolowicz@gmail.com>
2022-09-10 15:20:55 -0700

    Compass now includes level bounds

--- Rocketman/Assets/Scripts/UI/Compass.cs
+++ Rocketman/Assets/Scripts/UI/Compass.cs
@@ -7,6 +7,7 @@ public class Compass : MonoBehaviour
 {
     public RectTransform compassNeedle;
     public RectTransform compassSpan;
+    public RectTransform levelBounds;
 
     public Transform playerLoc;
     public Transform platformLoc;
@@ -15,16 +16,26 @@ public class Compass : MonoBehaviour
 
     float platformWidth;
 
+    void Start()
+    {
+        float stageWidth = boundaryR.localPosition.x - boundaryL.localPosition.x;
+        platformWidth = platformLoc.localScale.x;
+
+        float platformWidthPercentage = platformWidth / stageWidth;
+
+        levelBounds.sizeDelta = new Vector2(512, levelBounds.sizeDelta.y);
+        compassSpan.sizeDelta = new Vector2(platformWidthPercentage * 512, compassSpan.sizeDelta.y);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        platformWidth = platformLoc.localScale.x;
         float distance = platformLoc.localPosition.x - playerLoc.localPosition.x;
         distance += (platformWidth / 2);
         float distancePercentage = (distance / platformWidth) - (.5f);
 
         Debug.Log(distancePercentage);
 
-        compassNeedle.anchoredPosition = new Vector2(distancePercentage * -400, 150f);
+        compassNeedle.anchoredPosition = new Vector2(distancePercentage * -compassSpan.sizeDelta.x, 150f);
     }
 }
